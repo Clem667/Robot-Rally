18a67ade9ef17700f23f771a4530f04a95079f3d
yzasmin <74350354+yzasmin@users.noreply.github.com>
2022-04-10 23:40:07 +0200

    server

--- RobotRally667/src/server/Client2.java
+++ RobotRally667/src/server/Client2.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class Client {
+public class Client2 {
 	
 	public static void main (String [] args) throws UnknownHostException, IOException {
 		
