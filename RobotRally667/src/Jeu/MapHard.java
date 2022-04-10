ee61bfd83ee35aac5bebed999b3b1098ea8d5f3f
Clem667 <clement.ruiz1@etu.univ-montp3.fr>
2022-04-10 17:03:01 +0200

    bug map

--- RobotRally667/src/Jeu/MapHard.java
+++ RobotRally667/src/Jeu/MapHard.java
@@ -13,15 +13,10 @@ public class MapHard extends Map {
 			{"! ", "! ", "# ", "! ", "V ", "# ", "! ", "# ", "V ", "# "},
 			{"# ", "# ", "# ", "# ", "! ", "! ", "# ", "! ", "! ", "# "},
 			{"# ", "! ", "# ", "! ", "3 ", "# ", "! ", "# ", "# ", "! "}}; 
-<<<<<<< HEAD
-		
-	public MapHard() { // map difficile predefinie
-=======
 	
 	//rajouter les vies
 	
 	public MapHard() {
->>>>>>> branch 'master' of https://github.com/Clem667/Robot-Rally.git
 		SetNiveau(3);
 		tab = mapHard;
 	}
